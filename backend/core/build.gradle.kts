53a57730df17b4dbe21387dcc63333278d6d488e
user <kek@keke>
2026-02-21 00:37:14 +0300

    Мелстрой Андрей Бурим :heart: Виталий Феденко

--- backend/core/build.gradle.kts
+++ backend/core/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 
 dependencies {
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.flyway)
+    implementation(libs.postgresql)
 
     testImplementation(libs.spring.boot.starter.test)
 }

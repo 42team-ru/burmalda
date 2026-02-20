3b1a328655f636328dd0632e798188a7f7f47fad
user <kek@keke>
2026-02-21 00:39:09 +0300

    Мелстрой Андрей Бурим :heart: Виталий Феденко

--- backend/core/build.gradle.kts
+++ backend/core/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.flyway)
     implementation(libs.lombok)
+    annotationProcessor(libs.lombok)
 
     implementation(libs.flyway.database.postgresql)
 
